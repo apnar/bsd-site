6cbff7214251ab0d930a73111cb5b2c5b219168f
Josh Lukens <jlukens@botch.com>
2025-01-14 11:22:01 -0500

    first try at handling form submit

--- functions/api/submit.js
+++ functions/api/submit.js
@@ -2,5 +2,15 @@
  * POST /api/submit
  */
 export async function onRequestPost(context) {
-  // TODO: Handle the form submission
+  try {
+    let input = await context.request.formData();
+    let pretty = JSON.stringify([...input], null, 2);
+    return new Response(pretty, {
+      headers: {
+        "Content-Type": "application/json;charset=utf-8",
+      },
+    });
+  } catch (err) {
+    return new Response("Error parsing JSON content", { status: 400 });
+  }
 }
