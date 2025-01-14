442dadc7ada27358d9666f4c2673e66530fcfd10
Josh Lukens <jlukens@botch.com>
2025-01-14 12:29:52 -0500

    pretty up output

--- functions/api/submit.js
+++ functions/api/submit.js
@@ -4,13 +4,24 @@
 export async function onRequestPost(context) {
   try {
     let input = await context.request.formData();
-    let pretty = JSON.stringify([...input], null, 2);
+    // Convert FormData to JSON
+    // NOTE: Allows multiple values per key
+    let output = {};
+    for (let [key, value] of input) {
+      let tmp = output[key];
+      if (tmp === undefined) {
+        output[key] = value;
+      } else {
+        output[key] = [].concat(tmp, value);
+      }
+    }
+    let pretty = JSON.stringify(output, null, 2);
     return new Response(pretty, {
       headers: {
-        "Content-Type": "application/json;charset=utf-8",
+        'Content-Type': 'application/json;charset=utf-8',
       },
     });
   } catch (err) {
-    return new Response("Error parsing JSON content", { status: 400 });
+    return new Response('Error parsing JSON content', { status: 400 });
   }
 }
