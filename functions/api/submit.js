2cfdedb04dda85d64c911a84f4cdc0085d1d532a
Josh Lukens <jlukens@botch.com>
2025-01-15 18:59:52 -0500

    update function to handle all most fields

--- functions/api/submit.js
+++ functions/api/submit.js
@@ -8,15 +8,35 @@ export async function onRequestPost(context) {
 async function submitHandler(context) {
   const body = await context.request.formData();
 
-  const { firstname, lastname, email, phone, redirect } =
+  const { redirect, firstname, lastname, phone, email, gendermale, pronouns,
+    height_ft, height_in, age, birthdate, pairing_info, captian, experience,
+    note_to_directors, referby, emergencyinfo, tryoutweekone } =
     Object.fromEntries(body);
+  
+  const height = height_ft + "\'" + height_in + '"'; 
+  const missing = [];
+  const position = [];
 
   const reqBody = {
     fields: {
       "First Name": firstname,
       "Last Name": lastname,
-      "Email": email,
       "Phone": phone,
+      "Email": email,
+      "Male": gendermale,
+      "Pronoun": pronouns,
+      "Height": height,
+      "Age": age,
+      "Birthday": birthdate,
+      "Pairing Info": pairing_info,
+      "Captain": captian,
+      "Experience": experience,
+      "Position": position,
+      "Additional Info": note_to_directors,
+      "Refer": referby,
+      "Emergency Contact": emergencyinfo,
+      "Missing Dates": missing,
+      "Requested Week 1": tryoutweekone,
     },
   };
 
