7b38e82dbafa4afe8be7b25163ab9feadecae1ad
Josh Lukens <jlukens@botch.com>
2025-01-15 22:38:53 -0500

    debug

--- functions/api/submit.js
+++ functions/api/submit.js
@@ -6,12 +6,17 @@ export async function onRequestPost(context) {
 }
 
 async function submitHandler(context) {
+  try {
   const body = await context.request.formData();
 
   const { redirect, firstname, lastname, phone, email, gendermale, pronouns,
     mypronouns, height_ft, height_in, age, birthdate, pairing_info, captain,
     experience, note_to_directors, referby, emergencyinfo, tryoutweekone } =
     Object.fromEntries(body);
+
+// fold pronouns together
+  let combpronouns = pronouns;
+  if (pronouns === "Not Listed") { combpronouns = mypronouns; }
 
 // use dummy birthdate if its blank  
   let mybirthdate = '1970-01-01';
@@ -44,7 +49,7 @@ async function submitHandler(context) {
       "Phone": phone,
       "Email": email,
       "Male": (gendermale === 'true'),
-      "Pronoun": pronouns,
+      "Pronoun": combpronouns,
       "Height": height,
       "Age": age,
       "Birthdate": mybirthdate,
@@ -73,6 +78,11 @@ async function submitHandler(context) {
     },
   );
 
+  if (!resp.ok) {
+    // redirecting to error site
+    //return Response.redirect(redirect, 303);
+  }
+
     let pretty = JSON.stringify([...body], null, 2);
     pretty += JSON.stringify(resp);
     return new Response(pretty, {
@@ -82,6 +92,10 @@ async function submitHandler(context) {
     });
 
 
-//  return resp;
+  //return resp;
   //return Response.redirect(redirect, 303);
+  }
+  catch (error) {
+    console.error(error.message);
+  }
 };
