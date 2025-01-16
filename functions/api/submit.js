76240afc3289277ba401fd24e6d3f0e694214511
Josh Lukens <jlukens@botch.com>
2025-01-15 22:58:24 -0500

    clean up redirect

--- functions/api/submit.js
+++ functions/api/submit.js
@@ -83,17 +83,19 @@ async function submitHandler(context) {
     return Response.redirect(errorsite, 303);
   }
 
-    let pretty = JSON.stringify([...body], null, 2);
-    pretty += JSON.stringify(resp);
-    return new Response(pretty, {
-      headers: {
-        'Content-Type': 'application/json;charset=utf-8',
-      },
-    });
+  //debug - shows submitted values
+    //let pretty = JSON.stringify([...body], null, 2);
+    //pretty += JSON.stringify(resp);
+    //return new Response(pretty, {
+    //  headers: {
+    //    'Content-Type': 'application/json;charset=utf-8',
+    //  },
+    //});
 
+  //debug - shows airtable respose
+    //return resp;
 
-  //return resp;
-  //return Response.redirect(redirect, 303);
+  return Response.redirect(redirect, 303);
   }
   catch (error) {
     console.error(error.message);
